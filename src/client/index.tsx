5c12479067c142ead1cab13f0d8ebdb2964f29fb
Mark Liang <mark@vdr.tw>
2025-10-30 14:41:59 +0800

    feat: install tailwindcss and formater

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,116 +1,17 @@
 import { createRoot } from "react-dom/client";
-import { usePartySocket } from "partysocket/react";
-import React, { useState } from "react";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-  useParams,
-} from "react-router";
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import { nanoid } from "nanoid";
+import Home from "./pages/home";
 
-import {
-  names,
-  type ChatMessage,
-  type ChatMessageInput,
-  type Message,
-} from "../shared";
-
-function App() {
-  const [name] = useState(names[Math.floor(Math.random() * names.length)]);
-  const [messages, setMessages] = useState<Array<ChatMessage>>([]);
-  const { room } = useParams();
-
-  const socket = usePartySocket({
-    party: "chat",
-    room,
-    onMessage: (evt) => {
-      const message = JSON.parse(evt.data as string) as Message;
-      if (message.type === "add") {
-        const foundIndex = messages.findIndex((m) => m.id === message.id);
-        if (foundIndex === -1) {
-          // probably someone else who added a message
-          setMessages((messages) => [...messages, message as any]);
-        } else {
-          // this usually means we ourselves added a message
-          // and it was broadcasted back
-          // so let's replace the message with the new message
-          setMessages((messages) => {
-            return messages
-              .slice(0, foundIndex)
-              .concat(message as any)
-              .concat(messages.slice(foundIndex + 1));
-          });
-        }
-      } else if (message.type === "update") {
-        setMessages((messages) =>
-          messages.map((m) => (m.id === message.id ? (message as any) : m)),
-        );
-      } else {
-        setMessages(message.messages);
-      }
-    },
-  });
-
-  return (
-    <div className="chat container">
-      {messages.map((message) => (
-        <div key={message.id} className="row message">
-          <div className="two columns user">{message.user}</div>
-          <div className="ten columns">{message.content}</div>
-          {message.user_country}, {message.user_device}, {message.user_ip},{" "}
-          {message.created_at}
-        </div>
-      ))}
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          const content = e.currentTarget.elements.namedItem(
-            "content",
-          ) as HTMLInputElement;
-          const chatMessage: ChatMessageInput = {
-            id: nanoid(8),
-            content: content.value,
-            user: name,
-            role: "user",
-            user_avatar: "",
-          };
-          setMessages((messages) => [...messages, chatMessage as any]);
-          // we could broadcast the message here
-
-          socket.send(
-            JSON.stringify({
-              type: "add",
-              ...chatMessage,
-            } satisfies Message),
-          );
-
-          content.value = "";
-        }}
-      >
-        <input
-          type="text"
-          name="content"
-          className="ten columns my-input-text"
-          placeholder={`Hello ${name}! Type a message...`}
-          autoComplete="off"
-        />
-        <button type="submit" className="send-message two columns">
-          Send
-        </button>
-      </form>
-    </div>
-  );
-}
+import "./tailwindcss.css";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Navigate to={`/${nanoid()}`} />} />
-      <Route path="/:room" element={<App />} />
+      <Route path="/:room" element={<Home />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   </BrowserRouter>,
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/client/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
